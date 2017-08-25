e46037a0ddc95af3e518b9e996573c8f69381319
William Toscano <william_toscano_@hotmail.com>
2017-08-24 20:27:23 -0700

    Practiced Java for loops and arrays

---
+++ src/org/launchcode/java/studios/QuizTime/Question.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.studios.QuizTime;
+
+public abstract class Question {
+}
---
+++ src/org/launchcode/java/studios/QuizTime/QuestionMultiple.java
@@ -0,0 +1,4 @@
+package org.launchcode.java.studios.QuizTime;
+
+public class QuestionMultiple extends Question {
+}
